029e5cb887e2022324de4cfcad9f91e1dacb8569
Rodrigo <rodrigo.haus@gmail.com>
2015-03-04 18:59:07 -0300

    .

--- src/main/java/com/laricafood/resources/JerseyConfig.java
+++ src/main/java/com/laricafood/resources/JerseyConfig.java
@@ -2,15 +2,12 @@ package com.laricafood.resources;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.ws.rs.ApplicationPath;
-
 /**
  * Created by rodrigo.bacellar on 04/03/2015.
  */
-@ApplicationPath("/laricafood")
-public class ApplicationConfig extends ResourceConfig {
+public class JerseyConfig extends ResourceConfig {
 
-    public ApplicationConfig() {
+    public JerseyConfig() {
         register(EstabelecimentoResource.class);
     }
 }
